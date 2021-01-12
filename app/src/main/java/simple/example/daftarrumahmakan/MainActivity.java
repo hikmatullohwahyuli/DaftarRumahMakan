9f0b05cc79ef7473c62dbb240810506ac6b47dde
himatullohwahyuli <hikmatullohwahyuli@gmail.com>
2021-01-12 21:57:05 +0700

    Daftar Rumah Makan

---
+++ app/src/main/java/simple/example/daftarrumahmakan/MainActivity.java
@@ -0,0 +1,14 @@
+package simple.example.daftarrumahmakan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
+    }
+}
